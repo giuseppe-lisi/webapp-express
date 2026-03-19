486438b2b62c2b545cf5eccd5a1dc4a478554d75
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-03-19 17:15:17 +0100

    db correctly uses environmental variables now

--- data/db.js
+++ data/db.js
@@ -1,21 +1,20 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+};
 
 // Instantiates db connection
 const db = mysql.createConnection(dbConfig);
 
-const dbConfig = {
-    DB_HOST: process.env.DB_HOST,
-    DB_PORT: process.env.DB_PORT,
-    DB_USER: process.env.DB_USER,
-    DB_PASS: process.env.DB_PASS,
-    DB_NAME: process.env.DB_NAME
-}
-
 // Connects to db
-db.connect(err => {
+db.connect((err) => {
     if (err) {throw err;}
 
-    console.log('Connected to database');
+    console.log("Connected to database");
 });
 
 module.exports = db;
