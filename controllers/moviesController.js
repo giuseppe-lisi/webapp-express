d1acb5ddfee48d65134f9086d91f62c577165138
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-03-19 17:42:31 +0100

    funzione di SHOW mostra libri con le relative recensioni

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -11,12 +11,19 @@ function index(req, res) {
 
 function show(req, res) {
     const id = req.params.id;
-    const sqlQuery = 'SELECT * FROM movies WHERE id = ?'
-    
+    const sqlQueryBook = 'SELECT * FROM movies WHERE id = ?'
+    const sqlQueryReviews = 'SELECT * FROM reviews WHERE movie_id = ?'
 
-    db.query(sqlQuery, [id], (err, results) => {
+    db.query(sqlQueryBook, [id], (err, books) => {
         if (err) return res.status(500).json({ err: 'DB query error', message: err.message });
-        res.json(results);
+        if (books.lenght === 0) {return res.status(404).json({ message: 'Could not find that book' })};
+
+        db.query(sqlQueryReviews, [id], (err, reviews) => {
+            if (err) return res.status(500).json({ err: 'DB query error', message: err.message });
+
+            books[0].reviews = reviews;
+            return res.json(books);
+        })
     })
 }
 
