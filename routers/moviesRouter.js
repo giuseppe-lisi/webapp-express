ab729a115dbfc06cae1023fe64eda598a6bde9bf
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-03-28 17:28:10 +0100

    aggiunta rotta al controller per gestire la richiesta di POST

--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -4,5 +4,6 @@ const moviesController = require('../controllers/moviesController')
 
 router.get('/', moviesController.index);
 router.get('/:id', moviesController.show);
+router.post('/:id/reviews', moviesController.storeReviews)
 
 module.exports = router; 
